48041fd575e2bac6c7c4e969e310fc698c8fd473
Yoshio Mori <yoshio.mori@hotmail.com>
2022-09-16 14:31:48 -0300

    Correção de impressão

--- views/print_resultado.py
+++ views/print_resultado.py
@@ -4,7 +4,8 @@ from views.imprime_na_mesma_linha import imprime_na_mesma_linha
 
 
 def print_resultado(n_timeout, qnt_vitorias_por_tipo, turnos):
-    imprime_na_mesma_linha(f"Quantas partidas terminam por time out (1000 rodadas): {n_timeout}")
+    imprime_na_mesma_linha('')
+    print(f"Quantas partidas terminam por time out (1000 rodadas): {n_timeout}")
     print(f'Quantos turnos em média demora uma partida: {decimal.Decimal(sum(turnos)) / len(turnos)}')
     print(
         f'Qual a porcentagem de vitórias por comportamento dos jogadores: {";".join(f"{decimal.Decimal(v) / 300}({t})" for t, v in qnt_vitorias_por_tipo.items())}')
